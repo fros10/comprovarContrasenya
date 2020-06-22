57cef43428a9313229366afc8ac214059bfa925c
fros10 <fros10@xtec.cat>
2020-06-22 19:50:50 +0200

    Verificar contrasenya versió 0.2

--- Contrasenya.java
+++ Contrasenya.java
@@ -24,7 +24,9 @@ public class Contrasenya {
         String resultat = "Contrasenya correcta!!";    // Resultat de password valido
         int length = 0;                     // Variable per la longitud de la contrasenya
         int numCount = 0;                   // Variable pel número de números utilitzats
-        int letCount = 0;                   // Variable pel número de lletres utilitzades
+        int upperCount = 0;                 // Variable pel número de lletres majúscules utilitzades
+        int lowerCount = 0;                 // Variable pel número de lletres minúsucles utilitzades
+        int simbolCount = 0;                // Variable pel número de símbols utilitzats
 
         for (int x =0; x < Password.length(); x++) {
             char caracter = Password.charAt(x);
@@ -33,24 +35,40 @@ public class Contrasenya {
                 numCount ++;
             }
 
-            if (Character.isLetter(caracter)){
-                letCount ++;
+            if (Character.isUpperCase(caracter)){
+                upperCount ++;
+            }
+
+            if (Character.isLowerCase(caracter)){
+                lowerCount ++;
+            }
+
+            if (x > 0 && x +1 < Password.length() && !(Character.isLetterOrDigit(caracter))){
+                simbolCount ++;
             }
 
             length = (x + 1); // longitud total
 
         } // Final del bucle
 
-        if (numCount < 1){              // Revisa que contingui al menys 1 número
-            resultat = "La contrasenya ha de tenir al menys un número!";
+        if (numCount < 2){              // Revisa que contingui al menys 2 números
+            resultat = "La contrasenya ha de tenir al menys dos números!";
         }
 
-        if (letCount < 1) {             // Revisa que contingui al menys 1 lletra
-            resultat = "La contrasenya ha de tenir al menys una lletra!";
+        if (upperCount < 1) {             // Revisa que contingui al menys 1 lletra majúscula
+            resultat = "La contrasenya ha de tenir al menys una lletra majúscula!";
         }
 
-        if (length < 8){                // Revisa la longitud mínima de 8 caràcters
-            resultat = "La contrasenya ha de tenir una longitud mínima de 8 caràcters!";
+        if (lowerCount < 1) {             // Revisa que contingui al menys 1 lletra minúscula
+            resultat = "La contrasenya ha de tenir al menys una lletra minúscula!";
+        }
+
+        if (simbolCount < 1) {             // Revisa que contingui al menys 1 símbol que no estigui ni al principi ni al final
+            resultat = "La contrasenya ha de tenir al menys un caràcter especial que no estigui ni al principi ni al final!";
+        }
+
+        if (length < 10){                // Revisa la longitud mínima de 10 caràcters
+            resultat = "La contrasenya ha de tenir una longitud mínima de 10 caràcters!";
         }
 
             return (resultat);         // Retorna el valor resultat
